9524e7a0b0787fce5670c8bee8df6a7a058c6366
seyeong <seyoung7555@naver.com>
2021-08-10 00:49:01 +0900

    JWT 프로젝트 시작

---
+++ src/test/java/com/seyeong/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seyeong.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
